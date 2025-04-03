449dfc36c4cfaf9de4e078a2f2b9a9be3964e2d6
ValeriaCA9 <vale38646@gmail.com>
2025-04-03 14:25:56 -0600

    Create ejercicio10.c

---
+++ ejercicio10.c
@@ -0,0 +1,2 @@
+//Ejercicio 10
+//Escribir un programa en C que solicite numeros al usuario hasta que se hayan introducido 10 numero o la suma de todos los numeros leidos sea mayor a 100. Por ultimo mostrar un mensaje indicando que condicion se ha cumplido 
