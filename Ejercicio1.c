1ef53f1be79016360a815d7c370651938178e104
ValeriaCA9 <vale38646@gmail.com>
2025-04-03 14:40:20 -0600

    Update Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,1 +1,12 @@
-//Escriba un programa en C que muestreen pantalla los numeros enteros del 1 al 100
+//Escriba un programa en C que muestre en pantalla los numeros enteros del 1 al 100
+#include <stdio.h>
+
+void main()
+{
+    int  i;
+    
+    for(i = 1; i <= 100; i++)
+    {
+        printf("%d, ", i);
+    }
+}
