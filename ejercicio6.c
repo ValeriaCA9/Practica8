d9f75c52f015ad9c9ccc10aec8efeba84d43e2fc
ValeriaCA9 <vale38646@gmail.com>
2025-04-03 14:31:05 -0600

    Create ejercicio6.c

---
+++ ejercicio6.c
@@ -0,0 +1,2 @@
+//Escriba un programa que pida al usuario un numero, y muestre el mensaje "has introducido el numero__""(mostrando el numero que ha introducido):Esta accion debe repetirse hasta que el usuario introduzca el numero 0. 
+//En este momento se mostrara el mensaje "Finalizado: Se ha introducido el numero 0". Ademas, se debe mostrar cuantos numeros se han introducido y su suma.
