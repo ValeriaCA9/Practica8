d41586586f67cfc5c726471cbb61e55d14716f83
ValeriaCA9 <vale38646@gmail.com>
2025-04-03 14:21:10 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,31 @@
+//Cruz Alegria Valeria Montserrat
+//ES CON UN DO WHILE 
+//#include <stdio.h>
+
+int main()
+{
+    int n , num;
+    int par = 0, impar = 0;
+    printf("numero de pares e impares:");
+    scanf ("%d", &n);
+    int i = 1;
+    
+    while(i<=n);
+    {
+     if(num % 2==0)
+     {
+         par = i;
+     }
+     else
+     {
+         impar= i;
+     }
+     i++;
+    }
+    
+    if(n=0);
+    printf("numero par= %d\i" par);
+    printf ("numero impar= %d\i" impar);
+
+
+    
