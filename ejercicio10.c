4c1786dd22ddc3f426db1cad0fca3ecfc195536c
ValeriaCA9 <vale38646@gmail.com>
2025-05-16 12:16:27 -0600

    Update ejercicio10.c

--- ejercicio10.c
+++ ejercicio10.c
@@ -1,2 +1,14 @@
 //Ejercicio 10
 //Escribir un programa en C que solicite numeros al usuario hasta que se hayan introducido 10 numero o la suma de todos los numeros leidos sea mayor a 100. Por ultimo mostrar un mensaje indicando que condicion se ha cumplido 
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for(i = 1; i <= 100; i++) {
+        printf("%d, ", i);
+    }
+
+    return 0;
+}
